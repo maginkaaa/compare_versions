19e67f83cd70abc1ac64b149e6c3e5a96f91a3cd
Magdalena Karmashikova <magdalena.karmashikova@holoplot.com>
2022-02-09 14:43:54 +0100

    new tests

--- compareversions_test.go
+++ compareversions_test.go
@@ -43,7 +43,7 @@ func Test_compareVersions(t *testing.T) {
 		want bool
 	}{
 		{
-			name: "Test1",
+			name: "Test1 - Not compatible",
 			args: args{
 				matrix:        mm,
 				systemVersion: "1000",
@@ -52,7 +52,7 @@ func Test_compareVersions(t *testing.T) {
 			want: false,
 		},
 		{
-			name: "Test2",
+			name: "Test2 - Compatible",
 			args: args{
 				matrix:        mm,
 				systemVersion: "1001",
@@ -61,7 +61,7 @@ func Test_compareVersions(t *testing.T) {
 			want: true,
 		},
 		{
-			name: "Test3",
+			name: "Test3 - Not Compatible",
 			args: args{
 				matrix:        mm,
 				systemVersion: "1001",
@@ -70,7 +70,7 @@ func Test_compareVersions(t *testing.T) {
 			want: false,
 		},
 		{
-			name: "Test4",
+			name: "Test4 - Compatible",
 			args: args{
 				matrix:        mm,
 				systemVersion: "1128",
@@ -78,99 +78,29 @@ func Test_compareVersions(t *testing.T) {
 			},
 			want: true,
 		},
+		{
+			name: "Test5 - Broken Plan Version",
+			args: args{
+				matrix:        mm,
+				systemVersion: "1128",
+				planVersion:   "0.7,3",
+			},
+			want: false,
+		},
+		{
+			name: "Test6 - Broken System Version",
+			args: args{
+				matrix:        mm,
+				systemVersion: "1128Z",
+				planVersion:   "0.7,3",
+			},
+			want: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := compareVersions(tt.args.matrix, tt.args.systemVersion, tt.args.planVersion); got != tt.want {
+			if got := compatible(tt.args.matrix, tt.args.systemVersion, tt.args.planVersion); got != tt.want {
 				t.Errorf("compareVersions() = %v, want %v", got, tt.want)
-
-				/*
-
-					test4 := compareVersions(tt.args.matrix, "1128", "0.7.3") // should return True
-					if test4 != true {
-						t.Errorf("compareVersions() = %v, want %v", test4, true)
-					}
-
-					test5 := compareVersions(tt.args.matrix, "25567", "0.7.3.2") // should return False
-					if test5 != false {
-						t.Errorf("compareVersions() = %v, want %v", test5, false)
-					}
-
-					test6 := compareVersions(tt.args.matrix, "1375", "0.10.0") // should return True
-					if test6 != true {
-						t.Errorf("compareVersions() = %v, want %v", test6, true)
-					}
-
-					test7 := compareVersions(tt.args.matrix, "1375", "0.9.0") // should return False
-					if test7 != false {
-						t.Errorf("compareVersions() = %v, want %v", test7, false)
-					}
-
-					test8 := compareVersions(tt.args.matrix, "this is test", "0.9.0") // should return False
-					if test8 != false {
-						t.Errorf("compareVersions() = %v, want %v", test8, false)
-					}
-
-					test9 := compareVersions(tt.args.matrix, "this is test", "wrong") // should return False
-					if test9 != false {
-						t.Errorf("compareVersions() = %v, want %v", test9, false)
-					}
-
-					test10 := compareVersions(tt.args.matrix, "1289", "0.8.0") // should return False
-					if test10 != false {
-						t.Errorf("compareVersions() = %v, want %v", test10, false)
-					}
-
-					test11 := compareVersions(tt.args.matrix, "1289", "0.9.1") // should return True
-					if test11 != true {
-						t.Errorf("compareVersions() = %v, want %v", test11, true)
-					}
-
-					test12 := compareVersions(tt.args.matrix, "1375", "0.10.1") // should return True
-					if test12 != true {
-						t.Errorf("compareVersions() = %v, want %v", test12, true)
-					}
-
-					test13 := compareVersions(tt.args.matrix, "1110", "0.7.2") // should return False
-					if test13 != false {
-						t.Errorf("compareVersions() = %v, want %v", test13, false)
-					}
-
-					test14 := compareVersions(tt.args.matrix, "1125", "0.7.2") // should return True
-					if test14 != true {
-						t.Errorf("compareVersions() = %v, want %v", test14, true)
-					}
-
-					test15 := compareVersions(tt.args.matrix, "1001", "0.7.0") // should return False
-					if test15 != false {
-						t.Errorf("compareVersions() = %v, want %v", test15, false)
-					}
-
-					test16 := compareVersions(tt.args.matrix, "543872", "0.7.8") // should return False
-					if test16 != false {
-						t.Errorf("compareVersions() = %v, want %v", test16, false)
-					}
-
-					test17 := compareVersions(tt.args.matrix, "1125", "0.7.1") // should return False
-					if test17 != false {
-						t.Errorf("compareVersions() = %v, want %v", test17, false)
-					}
-
-					test18 := compareVersions(tt.args.matrix, "1289", "0.9.0") // should return True
-					if test18 != true {
-						t.Errorf("compareVersions() = %v, want %v", test18, true)
-					}
-
-					test19 := compareVersions(tt.args.matrix, "1290", "0.9.0") // should return True
-					if test19 != true {
-						t.Errorf("compareVersions() = %v, want %v", test19, true)
-					}
-
-					test20 := compareVersions(tt.args.matrix, "1289", "0.8.9") // should return False
-					if test20 != false {
-						t.Errorf("compareVersions() = %v, want %v", test20, false)
-					}*/
-
 			}
 		})
 	}
